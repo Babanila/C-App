8910ec926a38eaf36fc2d33e97582d3717d6e792
babanila <babanila@yahoo.com>
2019-05-01 10:42:40 +0200

    refactor(SavedRepositories): code restructure

--- frontend-react-app/src/components/SavedRepositories.js
+++ frontend-react-app/src/components/SavedRepositories.js
@@ -1,55 +1,51 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 
-export default class SavedRepositories extends Component {
-  constructor(props) {
-    super(props);
+const SavedRepositories = () => {
+  const [loading, setLoading] = useState(true);
+  const [savedRepositories, setSavedRepositories] = useState([]);
 
-    this.state = {
-      loading: true,
-      savedRepositories: []
+  useEffect(() => {
+    const getSavedRepo = async () => {
+      try {
+        const {
+          data: { bookmarkedRepositories }
+        } = await axios.get(`http://localhost:5000/save-repos`);
+        setSavedRepositories(bookmarkedRepositories);
+        setLoading(false);
+      } catch (_) {}
     };
-  }
+    getSavedRepo();
+  }, []);
 
-  async componentDidMount() {
-    const { data } = await axios.get(`http://localhost:5000/save-repos`);
-    this.setState({
-      loading: false,
-      savedRepositories: data
-    });
-  }
+  const repoData = items =>
+    items.map((item, i) => (
+      <Fragment key={item.id}>
+        <p> </p>
+        <h4> Item {i + 1}.</h4>
+        <li>id: {item.id}</li>
+        <li>node_id: {item.node_id}</li>
+        <li>name: {item.name}</li>
+        <li>private: {item.private}</li>
+        <li>description: {item.description}</li>
+        <li>url: {item.url}</li>
+      </Fragment>
+    ));
 
-  render() {
-    if (this.state.savedRepositories.length === 0)
-      return <h5>No saved repositories </h5>;
+  return (
+    <div>
+      <div>
+        <h3>Total saved Repositories: {savedRepositories.length}</h3>
+        <div>
+          {loading ? (
+            <h5> Loading ...</h5>
+          ) : (
+            <Fragment>{repoData(savedRepositories)}</Fragment>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    const repoData = this.state.savedRepositories.bookmarkedRepositories.map(
-      (item, i) => {
-        return (
-          <Fragment key={item.id}>
-            <p> </p>
-            <h4> Item {i + 1}.</h4>
-            <li>id: {item.id}</li>
-            <li>node_id: {item.node_id}</li>
-            <li>name: {item.name}</li>
-            <li>private: {item.private}</li>
-            <li>description: {item.description}</li>
-            <li>url: {item.url}</li>
-          </Fragment>
-        );
-      }
-    );
-    return (
-      <>
-        <>
-          <h3>
-            Total saved Repositories:
-            {this.state.savedRepositories.numberOfItems}
-          </h3>
-        </>
-
-        <>{repoData}</>
-      </>
-    );
-  }
-}
+export default SavedRepositories;
